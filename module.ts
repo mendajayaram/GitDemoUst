a7165efba5c0d4840b55078e5d7f0488a36060a7
= <=>
2021-03-18 17:45:32 +0530

    Module

--- module.ts
+++ module.ts
@@ -1,3 +1,3 @@
-import {position} from './oops';
+import {position} from './oops';//Jayaram
 var posobj=new position(1002,"jayaram",12345,"Full Stack Developer","Hyderabad");
 posobj.displayDetail();
